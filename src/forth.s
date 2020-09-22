be241221450453af902c5fcc4013777519d79369
Joel Heikkila <jjheik@gmail.com>
2020-09-22 02:17:37 -0400

    implemented some core words

--- src/forth.s
+++ src/forth.s
@@ -53,42 +53,6 @@ init:
 
         jmp     next
 
-
-.macro _lit value
-        .addr dolit
-        .word value
-.endmacro
-
-.macro _plus
-        .addr plus
-.endmacro
-
-.macro _halt
-
-        .addr halt
-.endmacro
-
-.macro _dup
-        .addr dup
-.endmacro 
-
-.macro _oneplus
-        .addr oneplus
-.endmacro
-
-code:
-
-        _lit $1111
-        .addr double
-         _halt
-
-double:
-        .addr docol
-        _dup
-        _plus
-        .addr exit
-
-
 docol:
         lda     fip+1
         pha
@@ -119,21 +83,14 @@ next:
         inc     fip+1               ; 5 (34)
     @1: jmp     zp_trampoline       ; 5 + 6 (45)
 
-.macro defcode name, previous, flags
-.ident (.concat (name, "_header")):
-    .word previous
-    
-    ;link .set (.word (.ident (.concat (name, "_header"))))
-
-    ;link .set (.word name)
-
-    
+.macro defcode name, previous, flags, label
+.ident (.concat (.string (label), "_header")):
+    .word previous 
     .byte flags + .strlen (name)
     .byte name
 
 .align 2
-.ident (name):   .word (.ident (name)) + 2
-
+.ident (.string(label)):   .word (.ident (.string(label))) + 2
 .endmacro
 
 .macro defword name, previous, flags
@@ -143,17 +100,19 @@ next:
     .byte name
 
 .align 2
-.ident (name):  .word(.ident (name)) + 2
+.ident (name):  .word docol
 .endmacro
 
-    defcode "exit", 0, 0
+    defcode "exit", 0, 0, exit
+    .define _exit .addr exit
         pla
         sta     fip
         pla
         sta     fip+1
         jmp     next
 
-    defcode "dup", exit, 0
+    defcode "dup", exit, 0, dup
+    .define _dup .addr dup
         lda     dstkbase_lo,x
         sta     dstkbase_lo-1,x
         lda     dstkbase_hi,x
@@ -161,12 +120,12 @@ next:
         dex
         jmp     next
 
-        
-    defcode "halt", dup, 0
-
+    defcode "halt", dup_header, 0, halt
+    .define _halt .addr halt 
         jmp *
 
-    defcode "plus", halt, 0
+    defcode "+", halt_header, 0, plus
+    .define _plus .addr plus
         clc
         lda     dstkbase_lo,   x
         adc     dstkbase_lo+1, x
@@ -177,7 +136,8 @@ next:
         inx
         jmp     next
 
-    defcode "dolit", plus, 0
+    defcode "lit", plus_header, 0, lit
+    .define _lit .addr lit
         dex
         ldy     #1
         lda     (fip),y
@@ -193,19 +153,83 @@ next:
         inc     fip+1
     @1: jmp     next
         
-    defcode "oneplus", dolit, 0
+    defcode "1+", lit_header, 0, oneplus
+    .define _oneplus .addr oneplus
         inc     dstkbase_lo,x
         bne     @1
         inc     dstkbase_hi,x
     @1: jmp     next
 
-    defcode "drop", oneplus, 0
+    defcode "drop", oneplus_header, 0, drop
+    .define _drop .addr drop 
         inx
         jmp     next
 
-    defcode "twodrop", drop, 0
+    defcode "2drop", drop_header, 0, twodrop
+    .define _twodrop .addr twodrop 
         inx
         inx
         jmp     next
 
-    
+    defcode "?dup", twodrop_header, 0, qdup
+    .define _qdup .addr qdup
+        lda     dstkbase_lo,x
+        ora     dstkbase_hi,x
+        beq     @1
+        lda     dstkbase_lo,x
+        sta     dstkbase_lo-1,x
+        lda     dstkbase_hi,x
+        sta     dstkbase_hi-1,x
+        dex
+    @1: jmp     next
+
+    defcode "=", qdup_header, 0, equal
+    .define _equal .addr equal
+        lda     dstkbase_lo,x
+        cmp     dstkbase_lo+1,x
+        bne     @1
+        lda     dstkbase_hi,x
+        cmp     dstkbase_hi+1,x
+        bne     @1
+        inx
+        lda     #$ff
+        sta     dstkbase_lo,x
+        sta     dstkbase_hi,x
+        jmp     next
+
+    @1: inx
+        lda     #$0
+        sta     dstkbase_lo,x
+        sta     dstkbase_hi,x
+        jmp     next
+
+    defcode "invert", equal_header, 0, invert
+    .define _invert .addr invert
+
+        lda     dstkbase_lo,x
+        eor     #$ff
+        sta     dstkbase_lo,x
+        lda     dstkbase_hi,x
+        eor     #$ff
+        sta     dstkbase_hi,x
+        jmp     next
+
+    defword "double", twodrop_header, 0
+    .define _double .addr double 
+        _dup
+        _plus
+        _exit
+
+code:
+
+        _lit 
+        .word $1111
+        _invert
+        _lit
+        .word $1112
+        _dup
+ 
+        _equal
+
+
+        _halt
